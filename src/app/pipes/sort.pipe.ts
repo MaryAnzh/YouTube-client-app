25e7458c4a96b5fc27b5f62ba009b7d084a7c807
MaryAnzh <MaryAnzh@mail.ru>
2022-04-04 01:20:31 +0400

    feat: add date sort on page

--- src/app/pipes/sort.pipe.ts
+++ src/app/pipes/sort.pipe.ts
@@ -10,7 +10,33 @@ export class SortPipe implements PipeTransform {
 
   transform(items: Item[]): Item[] {
 
+    items.sort((a, b) => {
+
+      if (SortPipe.itemAgeCalculated(a.snippet.publishedAt) > SortPipe.itemAgeCalculated(b.snippet.publishedAt)) {
+
+        return 1;
+      } else if (SortPipe.itemAgeCalculated(a.snippet.publishedAt) < SortPipe.itemAgeCalculated(b.snippet.publishedAt)) {
+
+        return -1;
+      } else {
+
+        return 0;
+      }
+    })
+
     return items;
 
   }
+
+  static itemAgeCalculated(exitDate: string): number {
+
+    const itemDate = Date.parse(exitDate);
+    const date = new Date();
+    const itemAgeInMilliseconds = +date - +itemDate;
+    const millisecondsInDay = 86400000;
+    const itemAgeInDay = itemAgeInMilliseconds / millisecondsInDay;
+
+    return itemAgeInDay;
+  }
+
 }
