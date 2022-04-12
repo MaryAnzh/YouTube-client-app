8e76b40357f36dec26dfb44b8f5e937fa129d095
MaryAnzh <MaryAnzh@mail.ru>
2022-04-12 15:38:47 +0400

    faet: add routerodule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './core/components/header/header.component';
+import { MainPageComponent } from './core/pages/main-page/main-page.component';
+import { NotFoundPageComponent } from './core/pages/not-found-page/not-found-page.component';
+import { AuthModule } from './auth/auth.module';
+
 
 const routes: Routes = [
-  { path: '', component: HeaderComponent },
-
+  { path: '', component: MainPageComponent },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  },
+  { path: '**', component: NotFoundPageComponent },
 ];
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
+import { AuthComponent } from './auth.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthComponent,
+    LoginPageComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ]
 })
+
 export class AuthModule { }
