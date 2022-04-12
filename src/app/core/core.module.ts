85da733f0f630297edce9bdb87fbb07e67d14c74
MaryAnzh <MaryAnzh@mail.ru>
2022-04-12 14:12:11 +0400

    faet: create pages componens

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { DetailedInformationPageComponent } from './pages/detailed-information-page/detailed-information-page.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    MainPageComponent,
+    LoginPageComponent,
+    NotFoundPageComponent,
+    DetailedInformationPageComponent
   ],
   imports: [
     CommonModule
