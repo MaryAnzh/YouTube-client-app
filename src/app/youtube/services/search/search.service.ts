f6fdbd96b0d963f393f44c7e37f9a7dd62f79977
MaryAnzh <MaryAnzh@mail.ru>
2022-04-18 01:08:19 +0400

    fix:

--- src/app/youtube/services/search/search.service.ts
+++ src/app/youtube/services/search/search.service.ts
@@ -10,26 +10,12 @@ export class SearchService {
 
   private baseURL = 'https://www.googleapis.com/youtube/v3/';
 
-  private search = 'search?';
-
-  private videos = 'videos?';
-
-  private APIkey1 = 'key=AIzaSyAPL2IqZBsGRYbZwj-jGWNZIhekt29YdUk'
-
-  private APIkey2 = 'key=AIzaSyCRGoNO1ZiwsjOE73FB4vF1owzaSUgcILs'
-    //'key=AIzaSyAPWskf1Z4RQ0j2CAVEVKTigC_siyCcSNg';
-  //AIzaSyCRGoNO1ZiwsjOE73FB4vF1owzaSUgcILs
-
-  private searchHeader = '&type=video&part=snippet&maxResults=15&q=';
-
-  private videoIdHeader = 'id=';
-
-  private videoHeaderPart = '&part=snippet,statistics'
+  private APIkey = 'AIzaSyAPL2IqZBsGRYbZwj-jGWNZIhekt29YdUk'
 
   constructor() { }
 
   async getYouTubeSearchResults(value: string) {
-    const response = await fetch((this.baseURL + this.search + this.APIkey1 + this.searchHeader + value), {
+    const response = await fetch((`${this.baseURL}search?key=${this.APIkey}&type=video&part=snippet&maxResults=15&q=${value}`), {
       method: 'GET',
     });
     const status = response.status;
@@ -46,7 +32,7 @@ export class SearchService {
 
   async getYouTubeVideo(id: String) {
 
-    const response = await fetch((`https://www.googleapis.com/youtube/v3/videos?id=${id}&key=AIzaSyCRGoNO1ZiwsjOE73FB4vF1owzaSUgcILs&part=snippet,statistics`), {
+    const response = await fetch((`${this.baseURL}videos?id=${id}&key=${this.APIkey}&part=snippet,statistics`), {
       method: 'GET',
     });
     const status = response.status;
