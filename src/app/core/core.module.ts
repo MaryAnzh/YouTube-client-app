0427debabd19b3a7153d951a7495c5c30050bb8f
MaryAnzh <MaryAnzh@mail.ru>
2022-04-20 17:21:32 +0400

    feat: add admin page layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    NotFoundPageComponent
+    NotFoundPageComponent,
+    AdminPageComponent
   ],
   imports: [
     CommonModule,
