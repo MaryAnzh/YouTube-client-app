de32a15689696993dd11bb0572b4a25f50f25819
MaryAnzh <MaryAnzh@mail.ru>
2022-04-25 23:47:56 +0400

    feat: add cardCreateForm: FormGroup

--- src/app/core/pages/admin-page/admin-page.component.ts
+++ src/app/core/pages/admin-page/admin-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-page',
@@ -8,5 +9,27 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class AdminPageComponent {
-
+  cardCreateForm: FormGroup = new FormGroup({
+    "title": new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+    "description": new FormControl('', [
+      Validators.required,
+      Validators.maxLength(225),
+    ]),
+    "imageLink": new FormControl('', [
+      Validators.required,
+      Validators.pattern(''),
+    ]),
+    "videoLink": new FormControl('', [
+      Validators.required,
+      Validators.pattern(''),
+    ]),
+    "creationDate": new FormControl('', [
+      Validators.required,
+      Validators.pattern(''),
+    ]),
+  });
 }
