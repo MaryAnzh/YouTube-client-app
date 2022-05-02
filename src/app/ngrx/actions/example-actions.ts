456f79dce0afc7eb803d8897618aac1d29753879
MaryAnzh <MaryAnzh@mail.ru>
2022-05-02 22:45:27 +0400

    feat: add ngrx

--- src/app/ngrx/actions/example-actions.ts
+++ src/app/ngrx/actions/example-actions.ts
@@ -1,13 +1,16 @@
 import { createAction, props } from "@ngrx/store";
+import { ICustomCard } from "src/app/youtube/model/custom-card.model";
+import { IVideoItem } from "src/app/youtube/model/video-item.model";
 
 export namespace ExampleActions {
 
   export const videoItemsAction = createAction(
     'Video Items',
-    props<{ search: string }>()
+    props<{ videoItems: IVideoItem[] }>()
   );
 
   const customCardsAction = createAction(
-    'Custom Cards'
+    'Custom Cards',
+    props<{ customCard: ICustomCard[] }>()
   )
 }
